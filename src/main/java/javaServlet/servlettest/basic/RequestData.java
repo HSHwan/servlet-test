1a798c3bf80d504f26377a93982aeff2b4375670
HSHwan <tnghks2866@gmail.com>
2024-04-04 15:25:26 +0900

    Add: request body json 데이터-> 자바 객체 등록 및 출력 서블릿 추가

---
+++ src/main/java/javaServlet/servlettest/basic/RequestData.java
@@ -0,0 +1,11 @@
+package javaServlet.servlettest.basic;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class RequestData {
+
+    private String username;
+    private int age;
+}
---
+++ src/main/java/javaServlet/servlettest/basic/request/RequestBodyJsonServlet.java
@@ -0,0 +1,35 @@
+package javaServlet.servlettest.basic.request;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletInputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javaServlet.servlettest.basic.RequestData;
+import org.springframework.util.StreamUtils;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "requestBodyJsonServlet", urlPatterns = "/request-body-json")
+public class RequestBodyJsonServlet extends HttpServlet {
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        String messageBody = StreamUtils.copyToString(inputStream, StandardCharsets.UTF_8);
+
+        System.out.println("messageBody = " + messageBody);
+
+        RequestData requestData = objectMapper.readValue(messageBody, RequestData.class);
+
+        System.out.println("requestData.username = " + requestData.getUsername());
+        System.out.println("requestData.age = " + requestData.getAge());
+
+        response.getWriter().write("ok");
+    }
+}
