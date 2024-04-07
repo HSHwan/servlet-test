1545363c4b7f90cfc51804bba58b40619624960c
HSHwan <tnghks2866@gmail.com>
2024-04-07 22:06:12 +0900

    Refactor: Controller 반환값을 ModelView에서 viewName으로 변환하여 controller 코드 간결화

---
+++ src/main/java/javaServlet/servlettest/web/frontcontroller/v4/ControllerV4.java
@@ -0,0 +1,13 @@
+package javaServlet.servlettest.web.frontcontroller.v4;
+
+import java.util.Map;
+
+public interface ControllerV4 {
+
+    /**
+     * @param paramMap
+     * @param model
+     * @return viewname
+     */
+    String process(Map<String, String> paramMap, Map<String, Object> model);
+}
---
+++ src/main/java/javaServlet/servlettest/web/frontcontroller/v4/controller/MemberListControllerV4.java
@@ -0,0 +1,21 @@
+package javaServlet.servlettest.web.frontcontroller.v4.controller;
+
+import javaServlet.servlettest.domain.member.Member;
+import javaServlet.servlettest.domain.member.MemberRepository;
+import javaServlet.servlettest.web.frontcontroller.v4.ControllerV4;
+
+import java.util.List;
+import java.util.Map;
+
+public class MemberListControllerV4 implements ControllerV4 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public String process(Map<String, String> paramMap, Map<String, Object> model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.put("members", members);
+        return "members";
+    }
+}
