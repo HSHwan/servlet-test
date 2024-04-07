f78f15ee995c9f3b88437b23805b7146a616ff5b
HSHwan <tnghks2866@gmail.com>
2024-04-07 20:31:38 +0900

    Refactor: 기존 Controller의 view rendering 공통 부분을 myView로 통합

---
+++ src/main/java/javaServlet/servlettest/web/frontcontroller/v2/controller/MemberListControllerV2.java
@@ -0,0 +1,25 @@
+package javaServlet.servlettest.web.frontcontroller.v2.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import javaServlet.servlettest.domain.member.Member;
+import javaServlet.servlettest.domain.member.MemberRepository;
+import javaServlet.servlettest.web.frontcontroller.MyView;
+import javaServlet.servlettest.web.frontcontroller.v2.ControllerV2;
+
+import java.io.IOException;
+import java.util.List;
+
+public class MemberListControllerV2 implements ControllerV2 {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    public MyView process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Member> members = memberRepository.findAll();
+        request.setAttribute("members", members);
+        return new MyView("/WEB-INF/views/members.jsp");
+    }
+}
